ed6ce698010ca9192d4a0e89d54722c50b9e25bd
YuriSoma <yaser.alelayan@outlook.com>
2025-05-14 19:00:32 +0000

    Upload Task-6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for n in i:
             print("{:d}".format(n), end=" ")
-        print("", end="")
+        print("\n", end="")
