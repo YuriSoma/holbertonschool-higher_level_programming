3e201e0f655ddf7c4f94f36fe520dd021c258dd3
YuriSoma <yaser.alelayan@outlook.com>
2025-05-17 20:27:27 +0000

    Upload Task-0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        tmp_matrix = []
+        for n in i:
+            tmp_matrix.append(n ** 2)
+        new_matrix.append(tmp_matrix)
+    
+    return new_matrix
