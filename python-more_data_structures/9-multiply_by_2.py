e0842d46b9fcbac328a98c2b563f37ffe35f1add
YuriSoma <yaser.alelayan@outlook.com>
2025-05-18 17:13:12 +0000

    Upload Task-9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
- def multiply_by_2(a_dictionary):
+def multiply_by_2(a_dictionary):
     keys_list = list(a_dictionary)
     new_dict = {}
     for i in keys_list:
         new_dict[i] = a_dictionary[i] * 2
-    return new_dict
+        return new_dict
