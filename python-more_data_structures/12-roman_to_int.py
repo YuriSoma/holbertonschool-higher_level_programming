36b8adb74079b16786c6489a249dc850cf4e4edf
YuriSoma <yaser.alelayan@outlook.com>
2025-05-18 20:25:41 +0000

    Upload Task-12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,34 +5,35 @@ def roman_to_int(roman_string):
     for i in range(len(str_list)):
         if str_list[i] == 'M':
             if i != 0 and str_list[i - 1] == 'C':
-                converted_count += 900
+                converted_count += 800
             else:
                 converted_count += 1000
         elif str_list[i] == 'D':
             if i != 0 and str_list[i - 1] == 'C':
-                converted_count += 400
+                converted_count += 300
             else:
                 converted_count += 500
         elif str_list[i] == 'L':
             if i != 0 and str_list[i - 1] == 'X':
-                converted_count += 40
+                converted_count += 30
             else:
                 converted_count += 50
         elif str_list[i] == 'V':
             if i != 0 and str_list[i - 1] == 'I':
-                converted_count += 4
+                converted_count += 3
             else:
                 converted_count += 5
         elif str_list[i] == 'C':
             if i != 0 and str_list[i - 1] == 'X':
-                converted_count += 90
+                converted_count += 80
             else:
                 converted_count += 100
         elif str_list[i] == 'X':
             if i != 0 and str_list[i - 1] == 'I':
-                converted_count += 9
+                converted_count += 8
             else:
                 converted_count += 10
         elif str_list[i] == 'I':
             converted_count += 1
     return converted_count
+print(roman_to_int("IV"))
