ce468455bd93bf0906eea1d0b49a51476aeb204d
YuriSoma3 <yaser.alelayan@outlook.com>
2025-05-21 06:14:49 +0000

    Fix pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,7 +10,7 @@ def add_integer(a, b=98):
     It will return the addition of these 2 inputs,
     if inputs aren't integer or float, then it will raise TypeError exception
     """
-    
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError('a must be an integer')
     if not isinstance(b, int) and not isinstance(b, float):
