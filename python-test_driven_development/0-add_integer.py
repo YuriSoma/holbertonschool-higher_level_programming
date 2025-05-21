efa7859359d2d61af91f3bf9b31c353649b05f4b
YuriSoma3 <yaser.alelayan@outlook.com>
2025-05-21 06:14:13 +0000

    Fix pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""
+""" addition Function """
+
+
+def add_integer(a, b=98):
+    """
     add_integer module expects to receive 2 integer
     (or float, to be converted later to int) inputs a and b
     (b has default value = 98).
     It will return the addition of these 2 inputs,
     if inputs aren't integer or float, then it will raise TypeError exception
-"""
-def add_integer(a, b=98):
-
-    """ addition Function """
+    """
+    
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError('a must be an integer')
     if not isinstance(b, int) and not isinstance(b, float):
