799d38f15540453f9f3c42f739e9ec31b81e2524
root <yaser.alelayan@outlook.com>
2025-05-21 22:23:52 +0000

    Upload Task-3

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -19,7 +19,7 @@ def print_square(size):
         raise TypeError('size must be an integer')
     elif size < 0:
         raise ValueError('size must be >= 0')
-    
+
     else:
         for i in range(size):
             for n in range(size):
