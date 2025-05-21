039e08b488938d2bad4aa4766d320e57d66bf62b
root <yaser.alelayan@outlook.com>
2025-05-21 18:48:19 +0000

    Fix pycodestyle Task-1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,7 +4,7 @@
 
 def matrix_divided(matrix, div):
     """
-    ...
+    
     """
 
     if not isinstance(div, int) and not isinstance(div, float):
@@ -19,15 +19,18 @@ def matrix_divided(matrix, div):
     if isinstance(matrix, list):
         for row in matrix:
             if not isinstance(row, list):
-                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+                raise 
+                TypeError('matrix must be a matrix (list of lists) of integers/floats')
             elif lastRowLen == 0:
                 lastRowLen = len(row)
             elif len(row) is not lastRowLen:
-                raise TypeError('Each row of the matrix must have the same size')
+                raise 
+                TypeError('Each row of the matrix must have the same size')
 
             for col in row:
                 if not isinstance(col, int) and not isinstance(col, float):
-                    raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+                    raise 
+                    TypeError('matrix must be a matrix (list of lists) of integers/floats')
                 else:
                     tmp_list.append(round((col / div), 2))
             matrix_result.append(tmp_list[:])
