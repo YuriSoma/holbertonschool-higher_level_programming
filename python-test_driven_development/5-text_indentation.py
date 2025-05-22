f2fa4283c8efafc781725d7d1dcdf6ecddb44e6d
YuriSoma <yaser.alelayan@outlook.com>
2025-05-22 17:20:44 +0000

    Upload Task-4

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" function that prints:
+    a text with 2 new lines after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    The function will get 1 input, text (Some string),
+    then prints text with 2 lines after each ., ? and :
+
+    If "text" is not string:
+    it will raise a TypeError if an input is not string.
+    Function shouldn't prints spaces at the beginning or at the end.
+    """
+
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+    
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
