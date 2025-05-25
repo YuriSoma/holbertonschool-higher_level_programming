bd9a8ddf8a4b245531f88ac5f41ac99c95ee94f2
YuriSoma <yaser.alelayan@outlook.com>
2025-05-25 14:22:27 +0000

    Update Task-3

---
+++ python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" A class that defines a square and initializes a private attribute
+    that will get a value (size) when an object created of the class.
+    The class also defines a class method that return the Square Area.
+
+    size has default value = 0.
+    Size must be int and >= 0, otherwise will raise an error:
+    TypeError: if size not integer
+    ValueError: if size < 0
+"""
+
+
+class Square:
+    """ A class defines Squares with initializing a private attribute """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return size ** 2
