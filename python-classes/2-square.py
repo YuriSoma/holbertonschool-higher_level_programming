906ae11991565a84c13e0a15d2c362945d1e5a26
YuriSoma <yaser.alelayan@outlook.com>
2025-05-25 14:08:28 +0000

    Upload Task-2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,7 +7,7 @@
 class Square:
     """ A class defines Squares with initializing a private attribute """
     def __init__(self, size=0):
-        if not isdigit(size):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
