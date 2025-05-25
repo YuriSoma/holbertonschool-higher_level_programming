6c46a85adb3b948ac12b6f1f0877fec1ea41af2c
YuriSoma <yaser.alelayan@outlook.com>
2025-05-25 19:57:14 +0000

    Upload Task-1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,7 +10,6 @@ class Rectangle:
         elif width < 0:
             raise ValueError('width must be >= 0')
         self.__width = width
-
         if not isinstance(height, int):
             raise TypeError('height must be an integer')
         elif height < 0:
