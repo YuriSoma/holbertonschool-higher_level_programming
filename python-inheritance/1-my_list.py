91a627b7e85b01825d513a33ada7f2b9a937a5b8
YuriSoma <yaser.alelayan@outlook.com>
2025-05-29 21:26:02 +0000

    Upload Task-1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A class that inherits form List """
+
+
+class MyList(list):
+    """
+    class inherits from list, then prints the list sorted
+    """
+    def print_sorted(self):
+        print(sort(self.list))
