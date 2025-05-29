6d6508aa44c3bdf90df30702739df57a1fe7fdde
YuriSoma <yaser.alelayan@outlook.com>
2025-05-29 18:25:00 +0000

    Upload Task-0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Function that returns a list object """
+
+
+def lookup(obj):
+    """
+    Receives an object then returns a list of available attributes and methods of it.
+    """
+
+    return dir(obj)
