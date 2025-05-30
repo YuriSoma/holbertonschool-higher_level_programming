0c404e2f6509b137b7af72bb1821bc3987228fee
YuriSoma <yaser.alelayan@outlook.com>
2025-05-30 14:49:00 +0000

    Upload Task-6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Defines a class with a method"""
+
+
+class BaseGeometry:
+    """
+    The method here raises an Exception with a message
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
