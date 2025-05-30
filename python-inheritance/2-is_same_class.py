11395dd37d6cdf945cf708cea6204a7216e8831f
YuriSoma <yaser.alelayan@outlook.com>
2025-05-30 12:20:00 +0000

    Fix Task-2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -11,6 +11,6 @@ def is_same_class(obj, a_class):
     otherwise returns False.
     """
 
-    if isinstance(type(obj), a_class):
+    if type(obj) is a_class:
         return True
     return False
