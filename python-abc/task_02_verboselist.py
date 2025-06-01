782c51f1f2c3cc2936074817224db53069b03ba9
root <yaser.alelayan@outlook.com>
2025-06-01 15:59:34 +0000

    Upload Task-2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module containing class Shape and subclasses Circle and Rectangle.
+"""
+
+
+class VerboseList(list):
+    """
+    A class to define a shape.
+    """
+    def append(self, item):
+        print("Added {} to the list.".format(item))
+        super().append(item)
+
+    def extend(self, iterable):
+        x = 0
+        for x in iterable:
+            x += 1
+        print("Extended the list with {} items.".format(x))
+        super().extend(iterable)
+
+    def remove(self, value):
+        print("Removed {} from the list.".format(value))
+        super().remove(value)
+
+    def pop(self, index=-1):
+        print("Popped {} from the list.".format(self[index]))
+        super().pop(index)
