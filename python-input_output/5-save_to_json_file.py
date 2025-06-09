e745c32b6ffa8fbdc6aa8b2588afa47e31dac686
YuriSoma <yaser.alelayan@outlook.com>
2025-06-09 12:30:16 +0000

    Fix docstring

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" A function convert an object to JSON file """
+""" A function saves an object to JSON file """
 
 import json
 """ Importing JSON module to convert an object to JSON """
