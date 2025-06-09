261e92ba1d8458ff8c5a4d31c81041a14e3bda4f
YuriSoma <yaser.alelayan@outlook.com>
2025-06-09 05:00:29 +0000

    Upload Task-0 Solution

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" A function reads given files """
+
+
+def read_file(filename=""):
+    """ The function will receive a file path, then prints its content.
+
+        Args:
+        filename - the file path (default value is empty string).
+
+        Return:
+        No returns, it just prints the reading of the file content.
+    """
+
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
