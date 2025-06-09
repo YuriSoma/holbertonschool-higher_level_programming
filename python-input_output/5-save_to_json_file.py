67ba9d882a51f390b9001f620a2cd71f88bf60a2
YuriSoma <yaser.alelayan@outlook.com>
2025-06-09 12:19:57 +0000

    Upload Task-5 solution

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" A function an object to JSON file """
+
+import json
+""" Importing JSON module to convert an object to JSON """
+
+
+def save_to_json_file(my_obj, filename):
+    """ The function will receive an object,
+        then save it to JSON file.
+
+        Args:
+        my_obj - An object
+
+        Return:
+        Nothing, just save the object to the JSON file.
+    """
+
+    with open(filename, "w") as f:
+        f.write(json.loads(my_obj))
