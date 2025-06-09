f58ea53d047de5776c544ad44a34bce1b0199358
YuriSoma <yaser.alelayan@outlook.com>
2025-06-09 11:25:30 +0000

    Fix the module documentation

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-import json
 """ A function that returns the JSON representation of an object """
+
+import json
+""" Importing JSON module to convert python to json strin """
 
 
 def to_json_string(my_obj):
