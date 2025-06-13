6091d15a7175290dc1ce0dca48ed36962e8ac6e5
YuriSoma <yaser.alelayan@outlook.com>
2025-06-13 15:35:27 +0000

    Upload Task-10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Student class
+"""
+
+
+class Student:
+    """ Creates instances of Students"""
+
+    def __init__(self, first_name, last_name, age):
+        """ Instantiation method """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Method that returns directory description """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            res = {}
+
+            for i in range(len(attrs)):
+                for x in obj:
+                    if attrs[i] == x:
+                        res[x] = obj[x]
+            return res
+
+        return obj
