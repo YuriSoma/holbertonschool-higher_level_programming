6b0e5fbf9b197f25c0f786aeb8fe2b75368d6676
YuriSoma <yaser.alelayan@outlook.com>
2025-06-13 15:05:59 +0000

    Fix 8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,9 +3,6 @@
     and then save them to a file.
 """
 
-import json
-""" Importing argv method to get all arguments """
-
 
 def class_to_json(obj):
     """ The function will receive an object.
@@ -14,7 +11,8 @@ def class_to_json(obj):
         obj - A class instance
 
         Return:
-        The dictionary description
+        The dictionary description of the obj
     """
 
-    return json.loads(obj)
+    if hasattr(obj, "__dict__"):
+        return obj.__dict__.copy()
