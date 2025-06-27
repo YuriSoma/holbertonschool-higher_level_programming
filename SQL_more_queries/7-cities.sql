a0c6c166d30f8856fa48119339917fc68f5deaa0
YuriSoma <yaser.alelayan@outlook.com>
2025-06-27 21:16:56 +0000

    Upload Task-7 solution

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- CREATE a DB and a table in it
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL FOREIGN KEY,
+    name VARCHAR(256) NOT NULL
+);
