4197b16f6d8f7e0c8ef9b4b188db7be86d67454f
YuriSoma <yaser.alelayan@outlook.com>
2025-06-27 20:57:44 +0000

    Upload Task-2 solution

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- CREATE a DB and a user
+-- GRANTS a privilege to the user for this DB
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
