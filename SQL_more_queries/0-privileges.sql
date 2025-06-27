744d1d7fbd277d0efd72821c117af6abc8c60902
YuriSoma <yaser.alelayan@outlook.com>
2025-06-27 20:36:36 +0000

    Fix Task-0 solution

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- SHOW privileges of some users
 
-SHOW GARNTS FOR 'user_0d_1'@'localhost'
-AND FOR 'user_0d_2'@'localhost';
+SHOW GARNTS FOR 'user_0d_1'@'localhost';
+SHOW GARNTS FOR 'user_0d_2'@'localhost';
