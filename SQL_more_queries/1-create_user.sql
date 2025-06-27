d708d70026aa3f1faf2f83c9ba19b4d718fdb9bf
YuriSoma <yaser.alelayan@outlook.com>
2025-06-27 20:49:43 +0000

    Fix Task-1 solution

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- CREATE some user with some privileges
 
-CREATE USER IF NO EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* FOR 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
