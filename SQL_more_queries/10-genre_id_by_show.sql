a4ac9cdfc7df14bcc7297dbed801668d58454f98
YuriSoma <yaser.alelayan@outlook.com>
2025-06-27 23:31:41 +0000

    Fix Task-10 solution

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,7 @@
 -- Lists all records of a table
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.genre_id=tv_show_genres.id
-ORDER BY tv_shows.title and tv_show_genres.genre_id;
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+        INNER JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
