cb5d3ffe659c3b262bc6b022029047230e984126
Your Name <you@example.com>
2025-07-13 17:33:18 +0000

    Upload Task-10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+function factorial (n) {
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(parseInt(argv[2])));
