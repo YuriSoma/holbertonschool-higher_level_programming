192611072de092d6ac443aa84d8da1f0c3d5d033
Your Name <you@example.com>
2025-07-13 14:54:13 +0000

    Upload Task-2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if (Object.keys(argv).length == 2) {
+if (Object.keys(argv).length === 2) {
   console.log('No argument');
-} else if (Object.keys(argv).length == 3) {
+} else if (Object.keys(argv).length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
