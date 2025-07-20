0d1ab0fcba4281fb20010910fe3640cd86ef5399
YuriSoma <106472235+YuriSoma@users.noreply.github.com>
2025-07-20 08:46:39 +0300

    Update task_02_logic.py

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -19,7 +19,7 @@ def contact():
 def get_items():
     with open('items.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
-    items = data[items]
+    items = data.get('items', [])
     return render_template('items.html', items = items)
 
 
